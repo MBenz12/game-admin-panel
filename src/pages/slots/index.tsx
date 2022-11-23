2bbff8cf012df2c4773fcbcf2bfdf40f0e4836f4
mbenz12 <mbenz10120112@gmail.com>
2022-11-23 08:56:53 -0500

    royalty field added

--- src/pages/slots/index.tsx
+++ src/pages/slots/index.tsx
@@ -295,7 +295,7 @@ export default function SlotsPage() {
     {
       setCommunityWallets([]);
       setNewCommunityWallets([default_community.toString()]);
-      setNewRoyalties([]);
+      setNewRoyalties([5]);
       setCommunityBalances([]);
     }
   }
